9d6eaada81d8ef737befc58c630119e75b261f7d
Jerome Leclanche <jerome@leclan.ch>
2016-10-12 16:49:59 +0300

    vagrant: Upgrade to Postgresql 9.5

--- scripts/provision_system.sh
+++ scripts/provision_system.sh
@@ -3,7 +3,9 @@
 echo 'deb http://cloudfront.debian.net/debian jessie-backports main
 deb-src http://cloudfront.debian.net/debian jessie-backports main' > /etc/apt/sources.list.d/backports.list
 echo "deb https://repos.influxdata.com/debian jessie stable" > /etc/apt/sources.list.d/influxdb.list
+echo "deb http://apt.postgresql.org/pub/repos/apt/ jessie-pgdg main" > /etc/apt/sources.list.d/postgres.list
 wget https://repos.influxdata.com/influxdb.key -qO - | apt-key add -
+wget https://www.postgresql.org/media/keys/ACCC4CF8.asc -qO - | apt-key add -
 
 apt-get update -q
 apt-get install -qy apt-transport-https
@@ -12,12 +14,13 @@ apt-get install -qy \
 	zsh curl git htop tree unzip vim \
 	python3 python3-dev python3-venv \
 	gcc g++ libxml2 libxml2-dev libxslt1-dev \
-	postgresql libpq-dev supervisor influxdb
+	supervisor influxdb
+apt-get install -qy -t jessie-backports postgresql-9.5 libpq-dev
 apt-get install -qy -t jessie-backports redis-server
 
 # Enable trust authentication for postgresql
-sed -i 's/all *postgres *peer/all postgres trust/' /etc/postgresql/9.4/main/pg_hba.conf
-systemctl restart postgresql
+sed -i 's/all *postgres *peer/all postgres trust/' /etc/postgresql/9.5/main/pg_hba.conf
+systemctl restart postgresql.service
 
 systemctl start influxdb
 
