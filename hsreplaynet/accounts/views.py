b16e850ca1c02a1c2a0c2a83ce57834127630b05
Jerome Leclanche <jerome@leclan.ch>
2016-10-23 14:10:03 +0300

    Add a setting to disable replay autoplay
    Touches #99

--- hsreplaynet/accounts/views.py
+++ hsreplaynet/accounts/views.py
@@ -15,7 +15,9 @@ from .models import AccountClaim, AccountDeleteRequest, User
 class EditAccountView(LoginRequiredMixin, UpdateView):
 	template_name = "account/edit.html"
 	model = User
-	fields = ["default_replay_visibility", "locale", "exclude_from_statistics"]
+	fields = [
+		"default_replay_visibility", "joust_autoplay", "locale", "exclude_from_statistics"
+	]
 	success_url = "/account/"
 
 	def get_object(self, queryset=None):
